039b8631456fd1fb6313c0c68e1327d9ecf351b8
guswns3198 <guswns3198@naver.com>
2023-01-18 21:39:23 +0900

    String

---
+++ BasicTypes/string.js
@@ -0,0 +1,9 @@
+"use strict";
+let myName = "James";
+myName = 'Mark';
+// Template String 사용 예시
+let fullName = "Yang Hyun Jun";
+let age = 28;
+let sentence = `Hello, my name is ${fullName}, I'm ${age} years old.`;
+
+console.log(sentence);
