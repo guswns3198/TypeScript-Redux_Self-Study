a001f08d3bc72998078fe37ba5abd913b4879e21
guswns3198 <guswns3198@naver.com>
2023-01-18 21:39:01 +0900

    Boolean

--- BasicTypes/boolean.js
+++ BasicTypes/boolean.js
@@ -1,6 +1,4 @@
-// npx tsc로 컴파일한 js 파일, node boolean.js로 console.log 값 확인 가능
-
 "use strict";
 let isDone = false;
 isDone = true;
-console.log(typeof isDone) // 'boolean'
+console.log(typeof isDone); // 'boolean'
