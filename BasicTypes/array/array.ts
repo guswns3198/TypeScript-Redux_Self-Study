b19b6d5beffa0c69989ecf3a8b909205d40d3be3
guswns3198 <guswns3198@naver.com>
2023-01-18 23:22:41 +0900

    BasicTypes

--- BasicTypes/array/array.ts
+++ BasicTypes/array/array.ts
@@ -2,6 +2,6 @@ let list: number[] = [1, 2, 3, 4, 5];
 
 let lists: Array<number> = [6, 7, 8];
 
-let mixList: (number | string)[] = [1, 2, 3, 4, "5"];
+let mixList: (number | string)[] = [1, "2", 3, 4, "5"];
 
 let mixLists: Array<number | boolean> = [6, 7, true];
