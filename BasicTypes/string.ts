d131df25e53a4294919713cead090eaebea269c3
guswns3198 <guswns3198@naver.com>
2023-01-18 21:39:31 +0900

    String

---
+++ BasicTypes/string.ts
@@ -0,0 +1,10 @@
+let myName: string = "James";
+
+myName = 'Mark';
+
+// Template String 사용 예시
+
+let fullName: string = "Yang Hyun Jun";
+let age: number = 28;
+
+let sentence: string = `Hello, my name is ${fullName}, I'm ${age} years old`;
