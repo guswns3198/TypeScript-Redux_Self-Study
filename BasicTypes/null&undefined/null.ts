c1fd7af04633ec440b8ecca5530a0a33a372b7df
guswns3198 <guswns3198@naver.com>
2023-01-18 22:33:45 +0900

    BasicTypes

---
+++ BasicTypes/null&undefined/null.ts
@@ -0,0 +1,13 @@
+// let MyName: string = null;
+
+// strictNullCheck가 true 이기에 위 식은 성립되지 않는다.
+
+// let u: undefined = null;
+
+let v: void = undefined;
+
+let union: string | null = null;
+
+// 위와 같은 union type 활용 가능
+
+union = "James";
