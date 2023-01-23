9cff8b6749fe043c18b80d8951d20d05fa78cc18
guswns3198 <guswns3198@naver.com>
2023-01-23 22:11:17 +0900

    Type System

---
+++ Type Sysyem/TypeSystem.ts
@@ -0,0 +1,29 @@
+//매개변수의 타입을 number로 지정
+// 함수의 리턴 타입은 명시적으로 지정하지 않았기 때문에 number로 추론
+
+function apple(a: number) {
+  if (a > 0) {
+    return a * 30;
+  }
+}
+
+console.log(apple(5)); // 150
+console.log(apple(-5)); // NaN
+
+// 해당 함수를 사용하는 사용자는 사용법에 맞게 숫자형을 사용하여 함수를 실행하였지만
+// 0 이상의 수를 입력하였을 때만 값이 올바르게 나오고 0 이하의 수를 입력시에 NaN가 나오게 됩니다.
+
+// 여기서 "strictNullChecks" 라는 옵션을 키게되면 모든 타입에 자동으로 포함되어 있는 'null', 'undefined'를 제거해줌
+
+// 밑에는 strictNullChecks를 켰을 때의 예시
+
+function apples(a: number) { // 매개변수의 타입을 명시적으로 지정
+  if (a > 0) { 
+    return a * 30; // 지정하지 않은 함수의 리턴 타입은 number | undefined로 추론
+  }
+}
+
+console.log(apple(5)); // 150, 사용자가 사용법에 맞게 숫자형을 사용하여 함수 실행
+console.log(apple(-5)); // undefined, 함수의 리턴 타입이 number | undefined 이기에 타입에 따른 연산을 바로 할 수 없다.
+
+// 해당 컴파일 에러를 고치기 위해 사용자와 작성자가 의논을 해야함
