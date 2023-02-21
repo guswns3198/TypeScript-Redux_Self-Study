21ccbf44a1138059c2c9948072d295904a126c0a
guswns3198 <guswns3198@naver.com>
2023-02-21 22:27:56 +0900

    Classes

---
+++ Classes/Static Properties & Methods.ts
@@ -0,0 +1,13 @@
+class Person15 {
+  public static CITY = "Bucheon";
+  public static hello() {
+    console.log("안녕하세요")
+  }
+}
+
+const p15 = new Person15();
+
+const p16 = new Person15();
+
+Person15.hello();
+Person15.CITY;
