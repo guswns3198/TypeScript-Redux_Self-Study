003a5d485864a3d304c89b01c423ba9f6c37a0ee
guswns3198 <guswns3198@naver.com>
2023-02-23 02:51:02 +0900

    Generics

---
+++ Generics/Generics Class.ts
@@ -0,0 +1,15 @@
+class PersonClass<T, K> {
+  private _name: T;
+  private _age: K;
+
+  constructor(name: T, age: K) {
+    this._name = name;
+    this._age = age;
+  }
+}
+
+new PersonClass("Yang", 28);
+
+new PersonClass<string, number>("Yang", 28);
+
+// 컴파일 타임에서 에러를 체크 가능하다.
